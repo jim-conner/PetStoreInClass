dfd0598ffc2df5d3352ea8b2de4071e80285788b
jim-conner <jamesdavidconner@gmail.com>
2021-07-20 18:21:56 -0500

    Add project files.

---
+++ PetStore/Pets/Dog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Pets
+{
+    class Dog //implicitly interal
+    {
+        //Access Modifiers:
+        //Private: only things in this class can use it
+        //^^private is default in a class 
+        //public: everything can access it
+        //internal: anything in this project can access it
+        //protected: anything that inherits from me can access
+
+        //Properties
+        public string Size { get; set; } //auto-property
+        public short Weight { get; private set; }
+        public string Name { get; set; }
+
+        //constructor method has to be same as class
+        //constructor or constructor method
+        public Dog(string name, short weight, string size)
+        {
+            Name = name;
+            Weight = weight;
+            Size = size;
+        }
+
+        //Method Signature
+        //consists of access modifier, return type, name, parameters
+        //void means not returning anything so absence of a type
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} barks loudly.");
+        }
+
+        //parameters are camelCase
+        public void Eat(string typeOfFood)
+        {
+            if (typeOfFood.ToLower() == "Healthy Food")
+            //if (string.Equals(typeOfFood, "healthy food",StringComparison.OrdinalIgnoreCase))
+            {
+                Weight -= 1;
+            }
+
+            Console.WriteLine($"{Name} just ate {typeOfFood}.  {Name} weighs {Weight}");
+        }
+    }
+}
---
+++ PetStore/Program.cs
@@ -0,0 +1,40 @@
+﻿using PetStore.Pets;
+using System;
+//order doesn't matter like it does for JS imports
+//using PetStore.Pets;
+
+namespace PetStore
+{
+    class Program
+    {
+        //static means shared across all instances
+        static void Main(string[] args)
+        {
+            //var melba = new Dog();
+            //melba.Name = "Melba";
+            //melba.Size = "Medium";
+            //melba.Weight = 60;
+            var melba = new Dog("Melba", 60, "Weight");
+            melba.Bark();
+
+            Console.WriteLine($"What kind of food should {melba.Name} eat?");
+            var typeOfFoodForMelba = Console.ReadLine();
+
+            melba.Eat(typeOfFoodForMelba);
+            //add a line here for user's input response
+
+            melba.Eat("healthy food");
+
+            var barley = new Dog("Barley", 60, "Medium");
+            //barley.Name = "Barley";
+            //barley.Size = "Medium";
+            //barley.Weight = 50;
+            barley.Bark();
+
+            barley.Eat("kitchen trash");
+
+            //this won't work bc can't dynamically add properties to a class in C#
+            //melba.Coat = "Short";
+        }
+    }
+}
